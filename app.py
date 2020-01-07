84be7a48f470460e15c0fdb366c4018f5b27172c
Raidin <raidin79@gmail.com>
2020-01-07 15:48:38 +0900

    Initial

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, url_for, jsonify, request, redirect
+from werkzeug import secure_filename
+
+
+# Flask 객체를 App 에 할당
+app = Flask(__name__)
+
+# app 객체를 이용해 라우팅 경로 설정 -> 실행 할 함수
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+@app.route('/json_test')
+def json_test():
+    data = {'name':'jihun', 'age':30}
+    return jsonify(data)
+
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['myName']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('myName')
+        return redirect(url_for('success', name=user))
+
+@app.route('/test', methods=['POST'])
+def test():
+    if request.method == 'POST':
+        data = request.get_json()
+        return jsonify(data)
+
+@app.route('/upload')
+def load_file():
+   return render_template('upload.html')
+
+@app.route('/upload_test', methods=['POST'])
+def upload_test():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save('./images/' + secure_filename(f.filename))
+        return 'file uploaded successfully'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8989')
